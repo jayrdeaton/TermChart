4fb8d22a4e47cfde74d58854f2c849a986b6163c
jaydeaton <jayrdeaton@gmail.com>
2019-04-22 12:32:34 -0500

    test script

---
+++ test.js
@@ -0,0 +1,8 @@
+const TermChart = require('./');
+
+const termChart = new TermChart([
+  { key: 'A', value: 100 },
+  { key: 'B', value: 150 },
+  { key: 'C', value: 175 },
+  { key: 'D', value: 200 }
+]);
