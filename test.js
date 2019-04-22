8ec31766c21382e7625741d5c380008bc934a08b
jaydeaton <jayrdeaton@gmail.com>
2019-04-22 13:48:52 -0500

    updated test

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-const TermChart = require('./');
+const TermChart = require('./'),
+  cosmetic = require('cosmetic');
 
 const termChart = new TermChart([
-  { key: 'A', value: 100 },
-  { key: 'B', value: 150 },
-  { key: 'C', value: 175 },
-  { key: 'D', value: 200 }
+  { key: 'Alpha', value: 20, character: '-' },
+  { key: 'Bravo', value: 30 , style: s => cosmetic.background.red(s) },
+  null,
+  { key: 'Charlie', value: 50, character: '+', style: s => cosmetic.green(s) },
+  { key: 'Delta' },
+  { value: 25 },
+  { key: 'Echo', value: 60 }
 ]);
+
+termChart.print();
